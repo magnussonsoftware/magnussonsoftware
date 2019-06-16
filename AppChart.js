0197adbe605cc6cb97dc2ef9370bbd37ce1de724
Johan Magnusson <magnusson@msoft.se>
2019-06-16 19:56:48 +0200

    chart working

--- AppChart.js
+++ AppChart.js
@@ -1,70 +1,134 @@
 import { html, LitElement } from 'https://unpkg.com/@polymer/lit-element@latest/lit-element.js?module';
-import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js'
+import 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.min.js'
 
-// Extend the LitElement base class
 class AppChart extends LitElement {
-
-    /**
-     * Implement `render` to define a template for your element.
-     *
-     * You must provide an implementation of `render` for any element
-     * that uses LitElement as a base class.
-     */
-
-
-
 
     firstUpdated(changedProperties) {
         var ctx = this.shadowRoot.querySelector('#myChart').getContext('2d');
+
+        // these are end values, above superior is olympic
+        var superior =      [ 70, 70, 65, 61, 56, 50 ].map( sup => VO2Max2Meters(sup));
+        var excellent =     [ 55.9, 52.4, 49.4, 48.0, 45.3, 44.2 ].map( sup => VO2Max2Meters(sup));
+        var good =          [ 39.0, 37.0, 35.7, 32.9, 31.5, 30.3 ].map( sup => VO2Max2Meters(sup));
+        var average =       [ 35.0, 33.0, 31.5, 29.0, 27.0, 24.5 ].map( sup => VO2Max2Meters(sup));
+        var poor =          [ 31.0, 29.0, 27.0, 24.5, 22.8, 20.2 ].map( sup => VO2Max2Meters(sup));
+        var veryPoor =      [ 25.0, 23.6, 22.8, 21.0, 20.2, 17.5 ].map( sup => VO2Max2Meters(sup));
+        
+        
+
+        function metersToVO2Max(value){
+            return Math.round((value - 504.9) / 44.73)
+        }
+        
+        function VO2Max2Meters(value){
+            return Math.round(value*44.73 + 504.9)
+        }
+        
+
         var myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+                datasets: [
+                    {
+                        label: 'Very poor',
+                        data: veryPoor,
+                        borderColor: "#BF001D",
+                        backgroundColor : "#BF001D",
+                    },
+                    {
+                        label: 'Poor',
+                        data: poor,
+                        borderColor: "#C43300",
+                        backgroundColor : "#C43300",
+                    },
+                    {
+                        label: 'Average',
+                        data: average,
+                        borderColor: "#C98900",
+                        backgroundColor : "#C98900",
+                    },
+                    {
+                        label: 'Good',
+                        data: good,
+                        borderColor: "#B9CE00",
+                        backgroundColor : "#B9CE00",
+                    },        
+                    {
+                        label: 'Excellent',
+                        data: excellent,
+                        borderColor: "#90D100",
+                        backgroundColor : "#90D100",
+                    },
+                    {
+                        label: 'Superior',
+                        data: superior,
+                        borderColor: "#0DD900",
+                        backgroundColor : "#0DD900",
+                    }          
+                    
+                ],
             },
             options: {
+                tooltips: {
+                    mode: 'nearest',
+                    intersect: false,
+                    callbacks: {
+                        label: function (tooltipItem, data) {
+                            var index = tooltipItem.datasetIndex >= data.datasets.length - 1 ? undefined : tooltipItem.datasetIndex + 1
+                            var prevIndex = tooltipItem.datasetIndex
+                            
+                            var max = ''
+                            var vo2Max = ''
+                            if(index ) {
+                                max = data.datasets[index].data[tooltipItem.index] 
+                                vo2Max = metersToVO2Max(max)
+                            }
+                                
+                            var min = data.datasets[prevIndex].data[tooltipItem.index] 
+                            return 'Range: '  + min + ' - ' + max + '  VO2Max: ' + metersToVO2Max(min) + ' - ' + vo2Max
+                        },
+                        //footer: function(tooltipItem, data) { return 'Total: 100 planos.'; }
+                    }
+                  },
                 scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Male runner\'s age'
+                        },
+                        type: 'category',
+                        labels: ['15', '25', '35', '45', '55', '65'],
+                        
+                    }],
                     yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Cooper\'s test Meters in 12 minutes'
+                        },
                         ticks: {
-                            beginAtZero: true
+                            min : 1300,
+                            max : 3400,
+                            stepSize: 100,
+                            
                         }
                     }]
-                }
+                }, 
             }
         });
+
     }
+
+    
 
     render() {
         return html`
-    <!-- template content -->
     <div>
-        <canvas id="myChart" width="400" height="400"></canvas>
-    
+        <canvas id="myChart" width="200" height="200"></canvas>
     </div>
     
     <p>A paragraph!</p>
     `;
     }
 }
-// Register the new element with the browser.
+
 customElements.define('app-chart', AppChart);
